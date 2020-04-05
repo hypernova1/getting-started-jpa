bab7646b22e23bc23a7532837b876d5c3e6fbb60
sam <chtlstjd01@gmail.com>
2020-04-05 23:37:05 +0900

    예외 처리

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -17,12 +17,18 @@ public class Main {
         EntityTransaction tx = em.getTransaction();// 트랜잭션 얻기
         tx.begin(); // 트랜잭션 시작
 
-        Member member = new Member();
-        member.setId(100L);
-        member.setName("sam");
-        em.persist(member); // 저장
+        try {
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("sam");
+            em.persist(member); // 저장
+            tx.commit(); // 커밋
 
-        tx.commit(); // 커밋
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         emf.close(); //종료
 
